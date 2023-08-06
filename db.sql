cba911b8eb9e390b1e86a7f5347e7eae2e9500c4
syraxxxx <rsitraka181@gmail.com>
2023-08-06 16:00:22 +0300

    change table event,site & add some data

--- db.sql
+++ db.sql
@@ -26,9 +26,9 @@ create table site
 (
     id          int auto_increment,
     label       varchar(100),
-    description varchar(100),
-    image       varchar(70),
-    video       varchar(70),
+    description text,
+    image       varchar(100),
+    video       varchar(100),
     idRegion    int,
     primary key (id),
     foreign key (idRegion) references user (id)
@@ -38,7 +38,7 @@ create table event
 (
     id          int auto_increment,
     label       varchar(50),
-    description varchar(50),
+    description text,
     date_event  date,
     primary key (id)
 );
@@ -76,8 +76,29 @@ INSERT INTO site VALUES (null, 'Île de Nosy Be', 'L''île de Nosy Be est une î
 INSERT INTO site VALUES (null, 'Parc national de l''Andringitra', 'Le Parc national de l''Andringitra est un parc montagneux qui offre des randonnées spectaculaires et des vues panoramiques.', 'andringitra.png', 'andringitra.mp4', 1);
 INSERT INTO site VALUES (null, 'Allée des Baobabs', 'L''Allée des Baobabs est une route bordée de majestueux baobabs, offrant un paysage emblématique de Madagascar.', 'allee_baobabs.png', 'allee_baobabs.mp4', 1);
 
-    insert
-into event
-values (null, 'Festival des baleines', 'Description avec des textes et des textes', '2023-08-14')
+INSERT INTO event 
+VALUES
+    (null, 'Festival des Lémuriens', 'Un festival célébrant la diversité des lémuriens malgaches et la préservation de leur habitat naturel.', '2023-08-06'),
+    (null, 'Fête de l''Indépendance', 'Célébration de l''indépendance de Madagascar avec des défilés, des spectacles culturels et des festivités.', '2023-08-10'),
+    (null, 'Festival de la Vanille', 'Un événement dédié à la célèbre vanille de Madagascar, avec des dégustations, des ateliers et des animations.', '2023-08-15'),
+    (null, 'Circuit des Tsingy', 'Une aventure passionnante à travers les formations rocheuses spectaculaires des Tsingy de Bemaraha.', '2023-08-20'),
+    (null, 'Festival du Riz', 'Une célébration de la culture malgache avec des danses, des chants et des plats traditionnels à base de riz.', '2023-08-25'),
+    (null, 'Festival du Baobab', 'Un festival mettant en avant l''emblématique arbre de baobab malgache avec des expositions et des activités.', '2023-08-30');
+
+INSERT INTO event 
+VALUES (null, 'Festival de la Vanille', 'Un événement dédié à la célèbre vanille de Madagascar, mettant en avant la culture, la cuisine et les utilisations de la vanille.', '2023-08-07');
+
+INSERT INTO event 
+VALUES (null, 'Course de Pirogues', 'Une compétition passionnante de pirogues traditionnelles malgaches, mettant en valeur l''habileté des navigateurs locaux.', '2023-08-08');
+
+INSERT INTO event 
+VALUES (null, 'Marché Artisanal', 'Un marché animé où les artisans malgaches présentent leurs créations, allant des sculptures en bois aux bijoux faits à la main.', '2023-08-09');
+
+
+INSERT INTO event 
+VALUES (null, 'Carnaval Malgache', 'Une célébration colorée avec des défilés, des danses, des costumes et de la musique, mettant en avant la diversité culturelle de Madagascar.', '2023-08-10');
+
+INSERT INTO event 
+VALUES (null, 'Excursion dans la Réserve de l''Isalo', 'Une aventure inoubliable à travers les canyons, les piscines naturelles et les formations rocheuses spectaculaires de la Réserve de l''Isalo.', '2023-08-11');
 
 insert into post values(null,'Mlay kely le baleine','image_url','video_url','2023-08-14 18:12:00',1)
