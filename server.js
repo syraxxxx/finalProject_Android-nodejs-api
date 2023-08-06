e2f24213e379cc8068effeeb5df5ec8aeeb95f96
Princia08 <princiaram38@yahoo.com>
2023-08-06 15:53:53 +0300

    db config

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ require("./app/routes/event.routes.js")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
